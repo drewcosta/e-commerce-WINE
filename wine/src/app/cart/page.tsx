073a8010480849247c95451a7a77b655e4a8924d
drewcosta <andrewcosta.contato@gmail.com>
2023-08-01 17:38:13 -0300

    feat: add cart page

---
+++ wine/src/app/cart/page.tsx
@@ -0,0 +1,169 @@
+'use client'
+import { BackBtn } from '@/components/Back-button'
+import { DefaultPageLayout } from '@/components/DefaultPageLayout'
+import { Divider } from '@/components/Divider'
+import { CartItems } from '@/components/cart/cart-items'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { ProductInCart } from '@/types/product'
+import styled from 'styled-components'
+
+export default function CartPage(){
+  const { value, updateLocalStorage } = useLocalStorage<ProductInCart[]>("cart-items", [])
+
+  const calculateTotal = (value: ProductInCart[]) => {
+    return value.reduce((sum, item) => sum += (item.price_in_cents * item.quantity), 0)
+  }
+
+  const cartTotal = calculateTotal(value).toFixed(2)
+  const delivery = 100
+  const cartTotalWithDelivery = calculateTotal(value) + delivery
+
+  const handleUpdateQuantity = (id: string, quantity: number) => {
+    const newValue = value.map(item => {
+      if( item.id != id) return item
+      return {...item, quantity: quantity}
+    })
+    updateLocalStorage(newValue)
+  }
+
+  const handleDeleteItem = (id: string) => {
+    const newValue = value.filter(item => {
+      if( item.id !== id) return item
+    })
+    updateLocalStorage(newValue)
+  }
+
+  return (
+    <DefaultPageLayout>
+      <Container>
+        <CartListContainer>
+          <BackBtn navigate='/' />
+          <h3>Seu carrinho</h3>
+          <p>
+            Total {value.length} produtos
+            <span> R$ {cartTotal}</span>
+          </p>
+          <CartList>
+            {value.map(item => 
+              <CartItems 
+                product={item} 
+                key={item.id} 
+                handleUpdateQuantity={handleUpdateQuantity}
+                handleDelete={handleDeleteItem}
+              />
+            )}
+          </CartList>
+        </CartListContainer>
+        <CartResultContainer>
+            <h3>Resumo do pedido</h3>
+            <TotalItem isBold={false}>
+              <p>Subtotal de produtos</p>
+              <p>{cartTotal}</p>
+            </TotalItem>
+            <TotalItem isBold={false}>
+              <p>Entrega</p>
+              <p>{delivery}.00</p>
+            </TotalItem>
+            <Divider />
+            <TotalItem isBold>
+              <p>Total</p>
+              {value.length > 0 ? <p>{cartTotalWithDelivery}</p> : '0.00'}
+            </TotalItem>
+            <ShopButton>finalizar compra</ShopButton>
+        </CartResultContainer>
+      </Container>
+    </DefaultPageLayout>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 32px;
+
+  @media(max-width: ${props => props.theme.tableBreakpoint}){
+    flex-direction: column;
+  }
+`
+
+const CartListContainer = styled.div`
+  width: 100%;
+  
+  h3{
+    font-size: 24px;
+    font-weight: 500;
+    text-transform: uppercase;
+    line-height: 300%;
+    margin-top: 24px;
+  }
+
+  p{
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 150%;
+
+    span{
+      font-weight: 700;
+    }
+  }
+`
+const CartList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  margin-top: 24px;
+`
+
+const CartResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  min-width: 352px;
+  padding: 16px 24px;
+  background: white;
+
+  h3{
+    margin-bottom: 30px;
+    font-size: 20px;
+    font-weight: 700;
+    color: var(--gray-darker);
+    text-transform: uppercase;
+  }
+`
+
+const TotalItem = styled.div<{ isBold: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 12px;
+
+  font-size: 16px;
+  font-weight: ${props => props.isBold ? '700' : '400'};
+  line-height: 150%;
+`
+
+const ShopButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.7rem 1.4rem;
+    width: 100%;
+    
+    color: #FFF;
+    background: var(--sucess-default);
+    font-size: 0.875rem;
+    font-weight: 700;
+    line-height: 0.97338rem;
+    text-transform: uppercase;
+    
+    border: none;
+    border-radius: 0.24338rem;
+    box-shadow: 0px 0.9733840227127075px 1.946768045425415px 0px rgba(0, 0, 0, 0.20);
+    cursor: pointer;
+`
+
---
+++ wine/src/components/cart/cart-items.tsx
@@ -0,0 +1,115 @@
+import { ProductInCart } from '@/types/product'
+import React, { ChangeEvent } from 'react'
+import styled from 'styled-components'
+
+interface CartProps{
+  product: ProductInCart
+  handleUpdateQuantity: (id: string, quantity: number) => void
+  handleDelete: (id: string) => void
+}
+
+export const CartItems = ({ product, handleUpdateQuantity, handleDelete }: CartProps) => {
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    handleUpdateQuantity(product.id, Number(e.target.value))
+  }
+
+  return (
+    <Item>
+      <button onClick={() => handleDelete(product.id)} aria-label="Deletar">
+        X
+      </button>
+      <img src={product.image_url} alt="" />
+      <div>
+        <h4>{product.name}</h4>
+        <p>{product.description}</p>
+        <div>
+          <SelectQuantity value={product.quantity} name="" id="" onChange={handleChange}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </SelectQuantity>
+          <span>{product.price_in_cents}</span>
+        </div>
+      </div>
+    </Item>
+  )
+}
+
+const Item = styled.li`
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 210px;
+  width: 100%;
+
+  border-radius: 8px;
+  background-color: white;
+
+  img{
+    max-height: 100%;
+    width: 8rem;
+    border-radius: 8px 0 0 8px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    padding: 16px 24px;
+
+    line-height: 150%;
+    color: var(--gray-darker);
+
+    h4{
+      font-size: 20px;
+      font-weight: 300;
+    }
+
+    p{
+      font-size: 12px;
+      font-weight: 400;
+      max-height: 50%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+
+      span{
+        font-size: 16px;
+        font-weight: 600;
+      }
+    }
+  }
+
+  button {
+        position: absolute;
+        top: 16px;
+        right: 24px;
+
+        border: none;
+        background: transparent;
+        cursor: pointer;
+    }
+`
+
+const SelectQuantity = styled.select`
+  padding: 8px;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 8px;
+  border: 1.5px solid var(--gray-default);
+  color: var(--gray-default);
+  background-color: var(--app-background);
+`
+
