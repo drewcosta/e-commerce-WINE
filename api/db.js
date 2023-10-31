be830e6903426fa7f142d99968166cb2f0bce750
Andrew Costa <andrewcosta.contato@gmail.com>
2023-10-30 21:11:27 -0300

    chore: removing unusable files

--- api/db.js
+++ api/db.js
@@ -4,163 +4,143 @@ const TOTAL_PAGES = 5;
 
 const baseProducts = [
   //tinto
-  { 
-    name: 'Altos Del Condor Cabernet Sauvignon Malbec 2022', 
-    description: 'Vinho jovem, corpo médio, frutado, com taninos marcantes e equilibrado', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=176,q=99/assets-images/produtos/29297-01.png', 
-    category: 'tinto', 
-    price_in_cents: 32.82
+  {
+    name: 'Altos Del Condor Cabernet Sauvignon Malbec 2022',
+    description: 'Vinho jovem, corpo médio, frutado, com taninos marcantes e equilibrado',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=176,q=99/assets-images/produtos/29297-01.png',
+    category: 'tinto',
   },
-  { 
-    name: 'Baron Philippe de Rothschild Mas Andes Cabernet Sauvignon 2021', 
-    description: 'Vinho jovem, corpo médio, frutado com toques de especiarias e herbáceo, taninos presentes e macios, acidez média, agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28632-01.png', 
-    category: 'tinto', 
-    price_in_cents: 58.71
+  {
+    name: 'Baron Philippe de Rothschild Mas Andes Cabernet Sauvignon 2021',
+    description: 'Vinho jovem, corpo médio, frutado com toques de especiarias e herbáceo, taninos presentes e macios, acidez média, agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28632-01.png',
+    category: 'tinto',
   },
-  { 
-    name: 'Príncipe de Viana D.O. Navarra Tempranillo 2020', 
-    description: 'Expressão frutada intensa na boca, perfeitamente apoiada por uma boa estrutura, taninos doces e um refrescante e durável final', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26579-01.png', 
-    category: 'tinto', 
-    price_in_cents: 70.47
+  {
+    name: 'Príncipe de Viana D.O. Navarra Tempranillo 2020',
+    description: 'Expressão frutada intensa na boca, perfeitamente apoiada por uma boa estrutura, taninos doces e um refrescante e durável final',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26579-01.png',
+    category: 'tinto',
   },
-  { 
-    name: 'Manos Negras Stone Soil Malbec 2018', 
-    description: 'Concentrado, intenso, taninos macios', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/25047-01.png', 
-    category: 'tinto', 
-    price_in_cents: 164.90
+  {
+    name: 'Manos Negras Stone Soil Malbec 2018',
+    description: 'Concentrado, intenso, taninos macios',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/25047-01.png',
+    category: 'tinto',
   },
-  { 
-    name: 'Colección De Familia La Mateo D.O.Ca. Rioja 2017', 
-    description: 'Encorpado, bem equilibrado, volumoso e com boa estrutura, frutado com nuances da barrica, taninos presentes, boa acidez, final longo', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26453-01.png', 
-    category: 'tinto', 
-    price_in_cents: 358.71
+  {
+    name: 'Colección De Familia La Mateo D.O.Ca. Rioja 2017',
+    description: 'Encorpado, bem equilibrado, volumoso e com boa estrutura, frutado com nuances da barrica, taninos presentes, boa acidez, final longo',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26453-01.png',
+    category: 'tinto',
   },
-  { 
-    name: 'Almaviva 2019', 
-    description: 'Encorpado, frutas maduras com nuances do tempo em barrica, opulento, suculento, volumoso, equilibrado, taninos polidos e firmes, acidez refrescante, final persistente e complexo', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/27495-01.png', 
-    category: 'tinto', 
-    price_in_cents: 1999.00
+  {
+    name: 'Almaviva 2019',
+    description: 'Encorpado, frutas maduras com nuances do tempo em barrica, opulento, suculento, volumoso, equilibrado, taninos polidos e firmes, acidez refrescante, final persistente e complexo',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/27495-01.png',
+    category: 'tinto',
   },
-  
+
   //branco
-  { 
-    name: 'Uma Wine Bar Chardonnay 2022', 
-    description: 'Vinho jovem, leve, bem equilibrado, frutado, boa acidez e agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28485-01.png', 
-    category: 'branco', 
-    price_in_cents: 35.18
+  {
+    name: 'Uma Wine Bar Chardonnay 2022',
+    description: 'Vinho jovem, leve, bem equilibrado, frutado, boa acidez e agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28485-01.png',
+    category: 'branco',
   },
-  { 
-    name: 'Casas del Toqui Single Estate Sauvignon Blanc 2022 375 mL', 
-    description: 'Vinho jovem, leve, frutado com toques herbáceos e minerais, acidez média para alta, agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/29026-01.png', 
-    category: 'branco', 
-    price_in_cents: 44.59
+  {
+    name: 'Casas del Toqui Single Estate Sauvignon Blanc 2022 375 mL',
+    description: 'Vinho jovem, leve, frutado com toques herbáceos e minerais, acidez média para alta, agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/29026-01.png',
+    category: 'branco',
   },
-  { 
-    name: 'Las Mercedes Singular Semillon 2020', 
-    description: 'Vinho jovem, leve, frutado, acidez média para alta, agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28346-01.png', 
-    category: 'branco', 
-    price_in_cents: 98.88
+  {
+    name: 'Las Mercedes Singular Semillon 2020',
+    description: 'Vinho jovem, leve, frutado, acidez média para alta, agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28346-01.png',
+    category: 'branco',
   },
-  { 
-    name: 'Monte da Pimenta I.G Alentejano Antao Vaz Arinto Verdelho Branco 2022', 
-    description: 'Personalidade forte, intenso, bom corpo e acidez, com final fresco e vigoroso', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28901-01.png', 
-    category: 'branco', 
-    price_in_cents: 117.53
+  {
+    name: 'Monte da Pimenta I.G Alentejano Antao Vaz Arinto Verdelho Branco 2022',
+    description: 'Personalidade forte, intenso, bom corpo e acidez, com final fresco e vigoroso',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28901-01.png',
+    category: 'branco',
   },
-  { 
-    name: 'Las Mercedes SKIN 2020', 
-    description: 'Corpo médio para encorpado, frutado com nuances herbáceas e de especiarias, bem estruturado, acidez média, final longo', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28347-01.png', 
-    category: 'branco', 
-    price_in_cents: 223.42
+  {
+    name: 'Las Mercedes SKIN 2020',
+    description: 'Corpo médio para encorpado, frutado com nuances herbáceas e de especiarias, bem estruturado, acidez média, final longo',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28347-01.png',
+    category: 'branco',
   },
 
   //espumante
-  { 
-    name: 'Espumante Ballade by Miolo Rosé Brut', 
-    description: 'Vinho jovem, leve, frutado, equilibrado, acidez vibrante e agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28525-01.png', 
-    category: 'espumante', 
-    price_in_cents: 56.35
+  {
+    name: 'Espumante Ballade by Miolo Rosé Brut',
+    description: 'Vinho jovem, leve, frutado, equilibrado, acidez vibrante e agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28525-01.png',
+    category: 'espumante',
   },
-  { 
-    name: 'Espumante Fantinel Cuvée Prestige Brut', 
-    description: 'Leve, fresco, floral, frutado, acidez agradável', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/10839-01.png', 
-    category: 'espumante', 
-    price_in_cents: 76.35
+  {
+    name: 'Espumante Fantinel Cuvée Prestige Brut',
+    description: 'Leve, fresco, floral, frutado, acidez agradável',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/10839-01.png',
+    category: 'espumante',
   },
-  { 
-    name: 'Espumante Amitié Viognier Brut', 
-    description: 'Fresco e tem ótima cremosidade, acidez e persistência gustativa marcante', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28147-01.png', 
-    category: 'espumante', 
-    price_in_cents: 129.29
+  {
+    name: 'Espumante Amitié Viognier Brut',
+    description: 'Fresco e tem ótima cremosidade, acidez e persistência gustativa marcante',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28147-01.png',
+    category: 'espumante',
   },
-  { 
-    name: 'Champagne Montaudon Brut', 
-    description: 'Cremoso, boa acidez, médio corpo, elegante', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/3170-01.png', 
-    category: 'espumante', 
-    price_in_cents: 341.06
+  {
+    name: 'Champagne Montaudon Brut',
+    description: 'Cremoso, boa acidez, médio corpo, elegante',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/3170-01.png',
+    category: 'espumante',
   },
 
   //rosé
-  { 
-    name: 'Altos Del Condor Rosé 2022', 
-    description: 'Vinho jovem, leve, frutado, com boa acidez e agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28482-01.png', 
-    category: 'rosé', 
-    price_in_cents: 32.82
+  {
+    name: 'Altos Del Condor Rosé 2022',
+    description: 'Vinho jovem, leve, frutado, com boa acidez e agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28482-01.png',
+    category: 'rosé',
   },
   {
-    name: 'Electum Tempranillo Rosé 2021', 
-    description: 'Vinho jovem, leve, frutado, suculento, acidez média para alta, agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28611-01.png', 
-    category: 'rosé', 
-    price_in_cents: 41.06
+    name: 'Electum Tempranillo Rosé 2021',
+    description: 'Vinho jovem, leve, frutado, suculento, acidez média para alta, agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28611-01.png',
+    category: 'rosé',
   },
-  { 
-    name: 
-    'Navaldar D.O.Ca Rioja Rosado 2020', 
-    description: 'Perfeito equilíbrio entre fruta e acidez, final delicado.', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26485-01.png', 
-    category: 'rosé', 
-    price_in_cents: 88.12
+  {
+    name:
+      'Navaldar D.O.Ca Rioja Rosado 2020',
+    description: 'Perfeito equilíbrio entre fruta e acidez, final delicado.',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26485-01.png',
+    category: 'rosé',
   },
-  { 
-    name: 'Sagrado D.O.C. Douro Rosé 2020', 
-    description: 'Vinho jovem, corpo leve para médio, volumoso, frutado, acidez média, agradável frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28954-01.png', 
-    category: 'rosé', 
-    price_in_cents: 141.06
+  {
+    name: 'Sagrado D.O.C. Douro Rosé 2020',
+    description: 'Vinho jovem, corpo leve para médio, volumoso, frutado, acidez média, agradável frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28954-01.png',
+    category: 'rosé',
   },
-  { 
-    name: 'Rosé à La Rosé A.O.C. Côtes de Provence Rosé 2020', 
-    description: 'Um vinho jovem, leve, frutado com nuances florais, acidez média para alta que lhe proporciona excelente frescor', 
-    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26691-01.png', 
-    category: 'rosé', 
-    price_in_cents: 258.71
+  {
+    name: 'Rosé à La Rosé A.O.C. Côtes de Provence Rosé 2020',
+    description: 'Um vinho jovem, leve, frutado com nuances florais, acidez média para alta que lhe proporciona excelente frescor',
+    image_url: 'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26691-01.png',
+    category: 'rosé',
   }
 ]
 
 const allProducts = new Array(TOTAL_PAGES).fill(1).reduce((acc) => {
   const products = baseProducts.map(product => ({
-    ...product, 
+    ...product,
     id: faker.datatype.uuid(),
-    // price_in_cents: faker.datatype.number({
-    //   min: 0,
-    //   max: 10000,
-    // }),
+    price_in_cents: faker.datatype.number({
+      min: 0,
+      max: 2000,
+    }),
     sales: faker.datatype.number(40),
     created_at: faker.date.past()
   })).sort(() => .5 - Math.random());
--- wine/src/types/priority-prices.ts
+++ wine/src/types/priority-prices.ts
@@ -1,7 +1,4 @@
 export enum PriorityPrices {
-  'ATE_40' = 'até R$40',
-  'R40_60' = 'R$40 A R$60',
-  'R100_200' = 'R$100 A R$200',
-  'R200_500' = 'R$200 A R$500',
-  'ACIMA_500' = 'Acima De R$500',
+  'MINOR_PRICE',
+  'BIGGEST_PRICE',
 }
