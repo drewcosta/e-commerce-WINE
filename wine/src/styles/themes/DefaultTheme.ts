2057043cab235b4c77b324c458521f5e3903bbf1
Andrew Costa <andrewcosta.contato@gmail.com>
2024-04-28 22:33:45 -0300

    feat: set default theme values

--- wine/src/styles/themes/DefaultTheme.ts
+++ wine/src/styles/themes/DefaultTheme.ts
@@ -1,14 +1,26 @@
 export const DefaultTheme = {
   colors: {
-
+    black: '#000000',
+    blackAlitteLight: '#1D1D1B',
+    grey: '#888888',
+    greyStronger: '#555555',
+    greyDark: '#333333',
+    greenMiddle: '#7EBC43',
+    pink: '#B6116E',
+    whiteBasic: '#FFFFFF',
   },
   backgroundColors: {
-
+    backgroundApp: '#F5F5F5',
   },
   fontSizes: {
-
+    text_sm: '14px',
+    text_base: '16px',
+    text_lg: '18px',
+    text_2xl: '24px',
+    text_6xl: '60px',
   },
-  Breakpoins: {
-
+  breakpoints: {
+    Mobile: '425px',
+    tablets: '768px',
   },
-}
+};
